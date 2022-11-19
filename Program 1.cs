aed74a225290151830610ed8ace4b9dc2a150f03
ZahraHosseiniFar <117304346+ZahraHosseiniFar@users.noreply.github.com>
2022-11-20 00:55:38 +0330

    Add files via upload

---
+++ Program 1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+    class Project
+    {
+        public static void Main()
+        {
+            string e = Console.ReadLine();
+            string f = Console.ReadLine();
+            string g = Console.ReadLine();
+            int a = Convert.ToInt32(e);
+            int b = Convert.ToInt32(f);
+            int c = Convert.ToInt32(g);
+            if (a + b > c && a + c > b && b + c > a)
+             mohit(a,b,c);
+            else
+               Console.WriteLine("NO");
+            Console.ReadKey();
+        }
+    
+    private static void mohit (int x , int y , int z )
+     {
+        int w = x * y * z;
+        Console.WriteLine(w);
+     }
+    
+    }//برنامه ای بنویسید ک سه عدد را خوانده و تشکیل یک مثلث دهند و محیط انرا چاپ کند
+
